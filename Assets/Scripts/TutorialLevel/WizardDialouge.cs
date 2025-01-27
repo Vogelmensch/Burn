acc6441a7354b176af76da15a78e3de3744f9deb
FrodoBeutlinAliasLeon <leon.krasniq@student.uni-tuebingen.de>
2025-01-27 20:12:44 +0100

    add WizardDialouge logic

--- Assets/Scripts/TutorialLevel/WizardDialouge.cs
+++ Assets/Scripts/TutorialLevel/WizardDialouge.cs
@@ -2,14 +2,47 @@ using TMPro;
 using UnityEngine;
 public class WizardDialogue : MonoBehaviour
 {
-    public GameObject dialogueBox; // Die Sprechblase (Parent-Objekt)
+    public GameObject dialogueBox; //Der Magier und die Sprechblase :))
     public TextMeshProUGUI dialogueText; // Das TMP-Textfeld für den Text
-    public Level1ChairDetector chairDetector;
-
+    private string[] dialogueLines1 = 
+    {
+        "Hello there, young travaler!",
+        "In this level you will need to take one of the chairs in front of you",
+        "Once you have done this, drag the chair onto the table to your left",
+        "Good luck!"
+    };
+    private string[] dialogueLines2 = 
+    {
+        "Great Job!",
+        "Level 2 is waiting!"
+    }
+    private
+    private int currentIndex = 0;
     private void Start()
     {
-        ShowDialogue("Hello there young traveler!", 5f);
+        ShowDialogue(dialogueLines1[currentIndex]);
     }
+
+    private void Update()
+    {
+        
+    }
+
+
+    public void NextDialogue()
+    {
+         if (currentIndex >= dialogueLines1.Length - 1)
+         {
+            HideDialogue();
+         }
+         else
+         {
+            currentIndex ++;
+            ShowDialogue(dialogueLines1[currentIndex]);
+         }
+    }
+
+
 
     public void ShowDialogue(string text)
     {
@@ -19,6 +52,6 @@ public class WizardDialogue : MonoBehaviour
 
     public void HideDialogue()
     {
-        dialogueBox.SetActive(false); // Sprechblase verstecken
+        dialogueBox.SetActive(false); 
     }
 }
