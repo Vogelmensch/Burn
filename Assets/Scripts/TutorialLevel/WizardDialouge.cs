ad10bdcd96f662801a0af16aea2799080daec02d
FrodoBeutlinAliasLeon <leon.krasniq@student.uni-tuebingen.de>
2025-01-27 20:56:56 +0100

    add logic

--- Assets/Scripts/TutorialLevel/WizardDialouge.cs
+++ Assets/Scripts/TutorialLevel/WizardDialouge.cs
@@ -4,41 +4,43 @@ public class WizardDialogue : MonoBehaviour
 {
     public GameObject dialogueBox; //Der Magier und die Sprechblase :))
     public TextMeshProUGUI dialogueText; // Das TMP-Textfeld für den Text
-    private string[] dialogueLines1 = 
+    public GameObject triggerObject;
+    private string[][] dialogueSets = 
+{
+    new string[] 
     {
-        "Hello there, young travaler!",
+        "Hello there, young traveler!",
         "In this level you will need to take one of the chairs in front of you",
         "Once you have done this, drag the chair onto the table to your left",
         "Good luck!"
-    };
-    private string[] dialogueLines2 = 
+    },
+    new string[] 
     {
         "Great Job!",
         "Level 2 is waiting!"
     }
-    private
+};
+
     private int currentIndex = 0;
+    private int currentArray = 0;
     private void Start()
     {
-        ShowDialogue(dialogueLines1[currentIndex]);
+        Collider collider = triggerObject.GetComponent<Collider>();
+        ShowDialogue(dialogueSets[currentArray][currentIndex]);
     }
-
-    private void Update()
-    {
-        
-    }
-
 
     public void NextDialogue()
     {
-         if (currentIndex >= dialogueLines1.Length - 1)
+         if (currentIndex >= dialogueSets[currentArray].Length - 1)
          {
             HideDialogue();
+            currentArray ++;
+            currentIndex = 0;
          }
          else
          {
             currentIndex ++;
-            ShowDialogue(dialogueLines1[currentIndex]);
+            ShowDialogue(dialogueSets[currentArray][currentIndex]);
          }
     }
 
@@ -54,4 +56,17 @@ public class WizardDialogue : MonoBehaviour
     {
         dialogueBox.SetActive(false); 
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Enter erkannt: " + other.gameObject.name);
+        if(other.CompareTag("MainCamera"))
+        {
+            ShowDialogue(dialogueSets[currentArray][currentIndex]);
+            Debug.Log("Kamera hat den Trigger betreten!");
+        }
+    }
+
+
+
 }
