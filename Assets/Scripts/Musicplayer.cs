6f285a4b744d48a003414ab1a01a8c7638b07eb3
Denis <denis.buss@student.uni-tuebingen.de>
2025-03-25 22:55:14 +0100

    jukebox zu jedem außer bonus hinzugefügt

--- Assets/Scripts/Musicplayer.cs
+++ Assets/Scripts/Musicplayer.cs
@@ -3,13 +3,57 @@ using System.Collections;
 
 public class Musicplayer : MonoBehaviour
 {
+  public AudioSource[] _audioSources;
+  public float musicduration;
+  public AudioClip currentClip;
+  public float delay;
+
+  private void Start()
+  {
+  }
+  private void Update()
+  {
+    playmusic();
+    //Volumechange((float)1.0);
+    //Debug.Log("1" + _audioSources[0].isPlaying);
+    //Debug.Log("2" + _audioSources[1].isPlaying);
+    
+
+  }
+
+  private void playmusic()
+  {
+    //1.spielt grade Musik?
+    //1.1 JA? do nothing
+    //1.2 Nein? aktivier eine der Musicdisks
+    //
+    if(!_audioSources[0].isPlaying && !_audioSources[1].isPlaying)
+    {
+      Debug.Log("damn daniel you went into the audios");
+      _audioSources[0].clip = currentClip;
+      _audioSources[0].PlayDelayed(delay);
+      Debug.Log(AudioSettings.dspTime);
+      _audioSources[1].clip = currentClip;
+      _audioSources[1].PlayDelayed(136 + delay);
+    }else
+    {
+      return;
+    }
+  }
+
+  public void Volumechange(float f)
+  {
+    _audioSources[0].volume = f;
+    _audioSources[1].volume = f;
+  }
   //public AudioSource audioSource;
+  /**
   public double musicDuration;
   public double goalTime;
   public AudioSource[] _audioSources;
   public int audioTogle;
   public AudioClip currentClip;
-
+  
   private void Update()
   {
     if(AudioSettings.dspTime > goalTime)
@@ -31,4 +75,5 @@ public class Musicplayer : MonoBehaviour
 
   }
   
+  **/
 }
